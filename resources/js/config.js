487ff8588e509e642327da9b77f3ebadb2cae5b4
Accident404 <mark2006klowie2020@gmail.com>
2026-09-11 11:45:44 +0800

    chat

--- resources/js/config.js
+++ resources/js/config.js
@@ -1,1 +1,11 @@
 const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx0yXSNWkVCPjNGn9fFT4HRkKLgh7CJnNek604Be08n-oY3PaEDJZIapCGZlvxrvJE/exec";
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6aa375751e12513447b8045a/1k2785lfv';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
